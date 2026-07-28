538772df1837ecbf3cae5c0ff474a0923c730977
alenap-a11y <alessandrelli.fr@gmail.com>
2026-07-28 09:07:46 +0200

    docs: trace correctif liaison Oracle Belline (espace residuel title)

---
+++ supabase/migrations/20260728102000_fix_liaison_belline_espace_residuel.sql
@@ -0,0 +1,5 @@
+-- Correction : "Oracle Belline" avait un espace invisible en fin de titre
+-- (16 caracteres au lieu de 14), rendant le WHERE title = 'Oracle Belline'
+-- muet dans la migration precedente. Liaison faite par id via SQL editor
+-- (f7f546e0-b74b-48a8-a312-a8b7496c3ad6). Fichier conserve pour tracabilite.
+SELECT 1;
